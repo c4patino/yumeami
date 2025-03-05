b923ee6b00e78dc30f044bab30011829761b6e4c
C4 Patino <c4patino@gmail.com>
2025-03-05 13:14:49 -0600

    fix(variety): locked variety package version to v0.8.12 latest working

--- home/wayland/services/variety.nix
+++ home/wayland/services/variety.nix
@@ -4,12 +4,23 @@
   inputs,
   config,
   ...
-}: {
+}: let
+  varietyLock = pkgs.variety.overrideAttrs (old: {
+    version = "0.8.12";
+    src = pkgs.fetchFromGitHub {
+      owner = "varietywalls";
+      repo = "variety";
+      tag = "0.8.12";
+      hash = "sha256-FjnhV7vzRPVDCgUNK8CHo3arKXuwe+3xH/5AxCVgeIY=";
+    };
+  });
+in {
   config = lib.mkIf config.hyprland.enable {
     home = {
+      # HACK: unlock variety in order to let it update as needed
       packages = with pkgs; [
         swaybg
-        variety
+        varietyLock
       ];
 
       file.".assets/desktops/" = {
