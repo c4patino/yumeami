5f1eae428f8ebfd8bb7d8c815562e2fbdfc211b9
C4 Patino <c4patino@gmail.com>
2025-05-05 23:42:21 -0500

    refactor: moved vaultwarden port from 5300 to 5400

--- system/hosts/global.nix
+++ system/hosts/global.nix
@@ -74,7 +74,7 @@
 
       vaultwarden = {
         host = "shiori";
-        port = 5300;
+        port = 5400;
       };
     };
 
--- system/services/vaultwarden.nix
+++ system/services/vaultwarden.nix
@@ -9,7 +9,7 @@
   cfg = config.vaultwarden;
   pgCfg = config.postgresql;
 
-  port = 5300;
+  port = 5400;
 
   resolveHostIP = node:
     if builtins.hasAttr node config.devices
