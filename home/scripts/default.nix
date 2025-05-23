a0a02e1f723587fb3849d4dd12c5902e05130a80
C4 Patino <c4patino@gmail.com>
2025-05-23 08:46:04 -0500

    feat(scripts): implemented remote nixos-rebuild switch script

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -3,5 +3,6 @@
     (import ./get-music-cover.nix {inherit pkgs;})
     (import ./scratchpad.nix {inherit pkgs;})
     (import ./format-drive.nix {inherit pkgs;})
+    (import ./nr.nix {inherit pkgs;})
   ];
 }
