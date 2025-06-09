f00d12ef1277dfab8299e33f797a55553f1e1cb8
C4 Patino <c4patino@gmail.com>
2025-06-08 19:50:37 -0500

    feat(shells): added cuda shell

---
+++ shells/cuda/default.nix
@@ -0,0 +1,21 @@
+{
+  pkgs ?
+    import <nixpkgs> {
+      config = {
+        allowUnfree = true;
+        cudaSupport = true;
+      };
+    },
+}:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    cudaPackages.cuda_nvcc
+    cudaPackages.cudatoolkit
+  ];
+
+  shellHook = ''
+    export CUDA_ROOT="${pkgs.cudaPackages.cudatoolkit}"
+  '';
+
+  shell = pkgs.zsh;
+}
