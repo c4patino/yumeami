02e7f676102cb8889c1f9a46a8e23eaca8203d46
C4 Patino <c4patino@gmail.com>
2025-06-16 21:02:12 -0500

    feat: added gcc and glibc to library path inputs

--- shells/c/default.nix
+++ shells/c/default.nix
@@ -27,6 +27,9 @@ mkShell {
   shell = pkgs.zsh;
 
   shellHook = ''
+    export LIBRARY_PATH="${pkgs.libgcc}/lib:$LIBRARY_PATH";
+    export LIBRARY_PATH="${pkgs.glibc}/lib:$LIBRARY_PATH";
+
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$NIX_LD_LIBRARY_PATH;
   '';
 }
--- shells/cpp/default.nix
+++ shells/cpp/default.nix
@@ -32,6 +32,9 @@ pkgs.mkShell {
   shell = pkgs.zsh;
 
   shellHook = ''
+    export LIBRARY_PATH="${pkgs.libgcc}/lib:$LIBRARY_PATH";
+    export LIBRARY_PATH="${pkgs.glibc}/lib:$LIBRARY_PATH";
+
     export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$NIX_LD_LIBRARY_PATH;
   '';
 }
