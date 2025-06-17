a8f9ce2614c4d9f1cd85078e7799135dc8ee232c
C4 Patino <c4patino@gmail.com>
2025-06-17 18:31:11 -0500

    feat: added haskell development environment

---
+++ shells/languages/haskell/default.nix
@@ -0,0 +1,25 @@
+{
+  pkgs ?
+    import <nixpkgs> {
+      config = {
+        allowUnfree = true;
+        cudaSupport = true;
+      };
+    },
+}:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    cabal-install
+    ghc
+  ];
+
+  packages = with pkgs; [
+    ormolu
+  ];
+
+  shell = pkgs.zsh;
+
+  shellHook = ''
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$NIX_LD_LIBRARY_PATH;
+  '';
+}
