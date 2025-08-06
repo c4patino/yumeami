eeac75203edba8eb73ee8e3dd5a57541177c21cd
C4 Patino <c4patino@gmail.com>
2025-08-06 15:01:05 -0500

    feat: added tlp to battery module to increase battery life

--- modules/nixos/hardware/battery/default.nix
+++ modules/nixos/hardware/battery/default.nix
@@ -16,5 +16,18 @@ in {
 
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [acpi];
+
+    services.tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+        STOP_CHARGE_THRESH_BAT0 = 1;
+      };
+    };
   };
 }
