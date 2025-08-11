970d5a406b420bf31e23dfa394bd9ee58ecc9698
C4 Patino <c4patino@gmail.com>
2025-08-11 08:26:14 -0500

    feat: added presenterm shell

---
+++ shells/presenterm/default.nix
@@ -0,0 +1,24 @@
+{
+  pkgs ?
+    import <nixpkgs> {
+      config = {
+        allowUnfree = true;
+        cudaSupport = true;
+      };
+    },
+}:
+pkgs.mkShell {
+  nativeBuildInputs = with pkgs; [
+    d2
+    mermaid-cli
+    pandoc
+    python313Packages.weasyprint
+    typst
+  ];
+
+  packages = with pkgs; [
+    presenterm
+  ];
+
+  shell = pkgs.zsh;
+}
