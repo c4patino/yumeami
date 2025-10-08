550f3c40c08a1e875c66603ca7fcb706aeae713f
C4 Patino <c4patino@gmail.com>
2025-10-08 12:14:06 -0500

    fix!: modified home.stateVersion and system.stateVersion to use 25.05 instead of 25.11

---
+++ systems/x86_64-linux/tobira/default.nix
@@ -0,0 +1,31 @@
+{
+  inputs,
+  lib,
+  namespace,
+  ...
+}: let
+  inherit (lib.${namespace}) enabled;
+in {
+  imports = [
+    ./hardware-configuration.nix
+
+    inputs.disko.nixosModules.default
+    (import ../../disko.nix {main = "/dev/sda";})
+  ];
+
+  ${namespace} = {
+    bundles = {
+      common = enabled;
+    };
+    services = {
+      networking.httpd = enabled;
+    };
+  };
+
+  networking = {
+    hostName = "shiori";
+    hostId = "asdfas";
+  };
+
+  system.stateVersion = "25.05";
+}
