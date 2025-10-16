6fc374a88d1f1b03f7a6fa08050b71216d32ade8
C4 Patino <c4patino@gmail.com>
2025-10-16 09:27:06 -0500

    fix: added remember user to tuigreet configuration

--- modules/nixos/desktop/env/greetd/default.nix
+++ modules/nixos/desktop/env/greetd/default.nix
@@ -17,7 +17,14 @@ in {
   config = mkIf cfg.enable {
     services.greetd = {
       enable = true;
-      settings.default_session.command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+      settings.default_session.command = ''
+        ${pkgs.tuigreet}/bin/tuigreet \
+          --time \
+          --remember \
+          --cmd Hyprland
+      '';
     };
+
+    ${namespace}.services.storage.impermanence.folders = ["/var/cache/tuigreet"];
   };
 }
