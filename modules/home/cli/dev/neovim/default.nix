ba32259cbafb2d73f87f0815e2634fcb474e33da
C4 Patino <c4patino@gmail.com>
2025-10-21 20:48:57 -0500

    feat: added yumevim variants to configuration to be done by-machine

--- modules/home/cli/dev/neovim/default.nix
+++ modules/home/cli/dev/neovim/default.nix
@@ -6,18 +6,23 @@
   system,
   ...
 }: let
-  inherit (lib) mkIf mkEnableOption;
+  inherit (lib) mkIf mkEnableOption mkOption;
   inherit (lib.${namespace}) getAttrByNamespace mkOptionsWithNamespace;
   base = "${namespace}.cli.dev.neovim";
   cfg = getAttrByNamespace config base;
 in {
-  options = mkOptionsWithNamespace base {
+  options = mkOptionsWithNamespace base (with lib.types; {
     enable = mkEnableOption "neovim";
-  };
+    variant = mkOption {
+      type = enum ["minimal" "full"];
+      description = "variation of yumevim to install";
+      default = "full";
+    };
+  });
 
   config = mkIf cfg.enable {
     home = {
-      packages = [inputs.yumevim-nix.packages.${system}.full];
+      packages = [inputs.yumevim-nix.packages.${system}.${cfg.variant}];
       sessionVariables.EDITOR = "nvim";
     };
   };
