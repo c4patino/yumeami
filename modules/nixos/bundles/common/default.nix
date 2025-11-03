9d425b547b4f80338bc8ba51ded1499cae756eea
C4 Patino <c4patino@gmail.com>
2025-11-03 10:10:32 -0600

    feat: added devenv cachix to substituters

--- modules/nixos/bundles/common/default.nix
+++ modules/nixos/bundles/common/default.nix
@@ -94,15 +94,17 @@ in {
       trusted-users = with config.users.users; [c4patino.name];
 
       substituters = [
-        "https://nix-community.cachix.org"
+        "https://cache.nixos.org"
+        "https://devenv.cachix.org"
         "https://hyprland.cachix.org"
-        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
       ];
 
       trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
 
