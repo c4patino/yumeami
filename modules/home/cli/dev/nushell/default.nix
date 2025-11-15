f70ad82bc2ac3c4e2ba1bf1d5a09f214e6cf2c09
C4 Patino <c4patino@gmail.com>
2025-11-15 10:04:57 -0600

    feat: added history fzf finder feature to nushell configuration

--- modules/home/cli/dev/nushell/default.nix
+++ modules/home/cli/dev/nushell/default.nix
@@ -20,9 +20,30 @@ in {
         enable = true;
         configFile.text = ''
           $env.config = {
-              buffer_editor: "nvim"
-              edit_mode: "vi"
-              show_banner: false
+            buffer_editor: "nvim"
+            edit_mode: "vi"
+            show_banner: false
+            keybindings: [
+              {
+                name: fuzzy_history_fzf
+                modifier: control
+                keycode: char_r
+                mode: [emacs , vi_normal, vi_insert]
+                event: {
+                  send: executehostcommand
+                  cmd: "commandline edit --replace (
+                    history
+                      | get command
+                      | reverse
+                      | uniq
+                      | str join (char -i 0)
+                      | fzf --scheme=history --read0 --tiebreak=chunk --layout=reverse --preview='echo {..}' --preview-window='bottom:3:wrap' --bind alt-up:preview-up,alt-down:preview-down --height=70% -q (commandline) --preview='echo -n {} | nu --stdin -c \'nu-highlight'''
+                      | decode utf-8
+                      | str trim
+                  )"
+                }
+              }
+            ]
           }
 
           # gitignore.io command
