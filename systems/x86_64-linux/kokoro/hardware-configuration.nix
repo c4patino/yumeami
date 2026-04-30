c14701bf5be47bd1b16807dda5e07af06789783c
C4 Patino <c4patino@gmail.com>
2026-04-30 00:25:34 -0500

    feat: update hardware-configuration.nix for new kokoro system

--- systems/x86_64-linux/kokoro/hardware-configuration.nix
+++ systems/x86_64-linux/kokoro/hardware-configuration.nix
@@ -8,12 +8,12 @@
   pkgs,
   ...
 }: {
-  imports = [(modulesPath + "/installer/scan/not-detected.nix")];
+  imports = [
+    (modulesPath + "/hardware/cpu/intel-npu.nix")
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
-  boot = {
-  };
-
-  boot.initrd.availableKernelModules = ["xhci_pci" "thunderbolt" "uas" "usb_storage" "sd_mod" "sdhci_pci"];
+  boot.initrd.availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = with pkgs.linuxPackages; [
@@ -22,17 +22,7 @@
 
   services.logind.settings.Login.HandleLidSwitchExternalPower = "ignore";
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-
-  # networking.interfaces.docker0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
-  # networking.interfaces.vboxnet0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp44s0.useDHCP = lib.mkDefault true;
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.npu.enable = true;
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
