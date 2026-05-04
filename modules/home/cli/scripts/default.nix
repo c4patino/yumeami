e66a91bd5a27e0abc39d1887e71f930652003942
C4 Patino <c4patino@gmail.com>
2026-05-04 15:20:45 -0500

    feat: removed nr script since it is no longer necessary with nh

--- modules/home/cli/scripts/default.nix
+++ modules/home/cli/scripts/default.nix
@@ -19,7 +19,6 @@ in {
       (import ./clock.nix {inherit pkgs;})
       (import ./format-drive.nix {inherit pkgs;})
       (import ./get-music-cover.nix {inherit pkgs;})
-      (import ./nr.nix {inherit pkgs;})
       (import ./scratchpad.nix {inherit pkgs;})
     ];
   };
