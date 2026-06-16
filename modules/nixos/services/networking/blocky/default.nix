06d8e40a1ba5bc7bf1fb158a93a3e1b2cd7169b4
C4 Patino <c4patino@gmail.com>
2026-06-16 11:35:04 -0500

    feat: filter down allowlists and denylists for blocky to hagezi list

--- modules/nixos/services/networking/blocky/default.nix
+++ modules/nixos/services/networking/blocky/default.nix
@@ -41,65 +41,21 @@ in {
             concurrency = 8;
             refreshPeriod = "4h";
           };
-          blackLists = {
-            ads = [
-              "https://blocklistproject.github.io/Lists/ads.txt"
-              "https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
-              "https://adaway.org/hosts.txt"
-              "https://v.firebog.net/hosts/AdguardDNS.txt"
-              "https://v.firebog.net/hosts/Admiral.txt"
-              "https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt"
-              "https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt"
-              "https://v.firebog.net/hosts/Easylist.txt"
-              "https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext"
-              "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts"
-              "https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts"
-              "https://raw.githubusercontent.com/AdGoBye/AdGoBye-Blocklists/main/AGBBase.toml"
-              "https://raw.githubusercontent.com/AdGoBye/AdGoBye-Blocklists/main/AGBCommunity.toml"
-              "https://raw.githubusercontent.com/AdGoBye/AdGoBye-Blocklists/main/AGBUpsell.toml"
-              "https://raw.githubusercontent.com/AdGoBye/AdGoBye-Blocklists/main/AGBSupporters.toml"
-            ];
-            tracking = [
-              "https://v.firebog.net/hosts/Easyprivacy.txt"
-              "https://v.firebog.net/hosts/Prigent-Ads.txt"
-              "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts"
-              "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt"
-              "https://hostfiles.frogeye.fr/firstparty-trackers-hosts.txt"
-            ];
-            malicious = [
-              "https://raw.githubusercontent.com/DandelionSprout/adfilt/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareHosts.txt"
-              "https://osint.digitalside.it/Threat-Intel/lists/latestdomains.txt"
-              "https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt"
-              "https://v.firebog.net/hosts/Prigent-Crypto.txt"
-              "https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts"
-              "https://v.firebog.net/hosts/RPiList-Phishing.txt"
-              "https://v.firebog.net/hosts/RPiList-Malware.txt"
-            ];
-            misc = [
-              "https://zerodot1.gitlab.io/CoinBlockerLists/hosts_browser"
-              "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-only/hosts"
-            ];
+          allowlists = {
             catchall = [
-              "https://big.oisd.nl/domainswild"
+              (pkgs.writeText "homelab.txt" ''
+                *.yumeami.sh
+                *.cpatino.com
+              '')
             ];
           };
-          whiteLists = let
-            customWhitelist = pkgs.writeText "misc.txt" ''
-              *.yumeami.sh
-            '';
-          in {
-            ads = [
-              "https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/whitelist.txt"
-              "https://raw.githubusercontent.com/anudeepND/whitelist/master/domains/optional-list.txt"
+          denylists = {
+            catchall = [
+              "https://cdn.jsdelivr.net/gh/hagezi/dns-blocklists@latest/wildcard/ultimate.txt"
             ];
-            misc = [customWhitelist];
           };
           clientGroupsBlock = {
             default = [
-              "ads"
-              "tracking"
-              "malicious"
-              "misc"
               "catchall"
             ];
           };
