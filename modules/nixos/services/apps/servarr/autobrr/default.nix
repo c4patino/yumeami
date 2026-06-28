8b47d116144dfd25d08d4fa95c816f5528846571
C4 Patino <c4patino@gmail.com>
2026-06-28 15:05:36 -0500

    refactor: separate out check-autobrr-space script from autobrr module

--- modules/nixos/services/apps/servarr/autobrr/default.nix
+++ modules/nixos/services/apps/servarr/autobrr/default.nix
@@ -51,35 +51,12 @@ in {
 
       services.autobrr = let
         autobrrUser = config.users.users.autobrr;
-
-        checkAutobrrSpace = pkgs.writeShellScriptBin "check-autobrr-space" ''
-          set -euo pipefail
-
-          if [ "$#" -ne 2 ]; then
-            echo "Error: Missing argument" >&2
-            exit 1
-          fi
-
-          parse_space() {
-            local space="''${1^^}"
-            space="''${space%B}"
-            ${pkgs.coreutils}/bin/numfmt --from=iec "$space"
-          }
-
-          required_space=$(parse_space "$1")
-          torrent_size="$2"
-          path="/mnt/nfs/servarr/torrents"
-
-          available_space=$(${pkgs.coreutils}/bin/df --output=avail -B1 "$path" | \
-            ${pkgs.gawk}/bin/awk 'END {print $1}')
-          remaining_space=$((available_space - torrent_size))
-
-          [ "$remaining_space" -gt "$required_space" ]
-        '';
       in
         mkMerge [
           {
-            path = [checkAutobrrSpace];
+            path = [
+              (import ./check-autobrr-space.nix {inherit pkgs;})
+            ];
             serviceConfig = {
               DynamicUser = mkForce false;
               User = autobrrUser.name;
