7a8725e90ebc96fec7a69cb14ad50694beae5245
C4 Patino <c4patino@gmail.com>
2026-07-03 12:21:49 -0500

    fix: modified samba to be created with 750 perms

--- modules/nixos/services/storage/samba/default.nix
+++ modules/nixos/services/storage/samba/default.nix
@@ -75,7 +75,14 @@ in {
     };
 
     ${namespace}.services.storage.impermanence.folders = mkMerge [
-      [ (mkPersistRootDir config "/var/lib/samba") ]
+      [
+        {
+          directory = "/var/lib/samba";
+          user = "root";
+          group = "root";
+          mode = "750";
+        }
+      ]
       (mkIf (cfg.shares != []) (cfg.shares |> map (s: mkPersistRootDir config "/mnt/samba/${s}")))
     ];
   };
