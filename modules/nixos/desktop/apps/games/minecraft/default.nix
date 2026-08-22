23098b5d898f1976cef91b82917619e1e7ca7a66
C4 Patino <c4patino@gmail.com>
2026-08-22 09:23:21 -0500

    feat: improve minecraft server to allow loading modpacks

--- modules/nixos/desktop/apps/games/minecraft/default.nix
+++ modules/nixos/desktop/apps/games/minecraft/default.nix
@@ -6,10 +6,46 @@
   pkgs,
   ...
 }: let
-  inherit (lib) flatten mapAttrs mapAttrsToList mkEnableOption mkIf mkOption types;
+  inherit (lib) filterAttrs flatten hasSuffix mapAttrs mapAttrsToList mkEnableOption mkIf mkMerge throwIf types;
   inherit (lib.${namespace}) getAttrByNamespace mkOpt mkOptAttrset mkOptionsWithNamespace mkPersistRootDir;
   base = "${namespace}.desktop.apps.games.minecraft";
   cfg = getAttrByNamespace config base;
+
+  nmLib = inputs.nix-minecraft.lib;
+  nmPkgs = pkgs.extend inputs.nix-minecraft.overlay;
+
+  manifestFiles = [
+    "index.json"
+    "modrinth.index.json"
+    "pack.toml"
+    "packwiz.json"
+  ];
+
+  detectPackType = src:
+    if hasSuffix ".mrpack" (toString src)
+    then "modrinth"
+    else if lib.pathExists (src + "/pack.toml")
+    then "packwiz"
+    else "plain";
+
+  installerArgs = pack: {
+    inherit (pack) src packHash side;
+  };
+
+  fetchers = {
+    modrinth = pack: nmPkgs.fetchModrinthModpack (installerArgs pack);
+    packwiz = pack: nmPkgs.fetchPackwizModpack (installerArgs pack);
+    plain = pack: pack.src;
+  };
+
+  fetchModpack = pack:
+    throwIf (pack.src == null) "modpack.src must be set."
+    (fetchers.${detectPackType pack.src} pack);
+
+  modpackSymlinks = pack:
+    fetchModpack pack
+    |> nmLib.collectFiles
+    |> filterAttrs (name: _: !(builtins.elem name manifestFiles));
 in {
   imports = [inputs.nix-minecraft.nixosModules.minecraft-servers];
 
@@ -18,10 +54,28 @@ in {
       enable = mkEnableOption "Minecraft Server";
       servers = mkOptAttrset (submodule {
         options = {
-          package = mkOpt package null "The Minecraft server package to use.";
+          package = mkOpt package (throw "servers.<name>.package must be set.") "The Minecraft server package to use.";
           jvmOpts = mkOpt str "-Xms4092M -Xmx4092M -XX:+UseG1GC" "JVM options for the Minecraft server.";
+
           serverProperties = mkOpt attrs {} "Minecraft server properties.";
           whitelist = mkOpt attrs {} "Whitelist for the Minecraft server.";
+
+          modpack =
+            mkOpt (nullOr (submodule {
+              options = {
+                src = mkOpt (nullOr path) null ''
+                  Modpack source: a .mrpack archive, a packwiz directory
+                  (with pack.toml), or a prebuilt tree (e.g. pkgs.fetchzip of
+                  a server-pack zip).
+                '';
+                packHash = mkOpt str lib.fakeHash "SHA-256 of the fetched pack tree; leave fakeHash to discover it on first build.";
+                side = mkOpt (enum ["server" "client" "both"]) "server" "Which side's files to fetch from the pack.";
+              };
+            }))
+            null "Modpack to deploy into the server directory.";
+
+          symlinks = mkOpt attrs {} "Extra files to symlink, overriding the modpack contents.";
+          files = mkOpt attrs {} "Extra writable files to copy, overriding the modpack contents.";
         };
       }) {} "Minecraft server configurations.";
     };
@@ -31,7 +85,7 @@ in {
 
     networking.firewall.allowedTCPPorts =
       cfg.servers
-      |> mapAttrsToList
+      |> mapAttrsToList (_: server: server.serverProperties.server-port or 25565)
       |> flatten;
 
     nixpkgs.overlays = [inputs.nix-minecraft.overlay];
@@ -42,9 +96,15 @@ in {
 
       servers =
         cfg.servers
-        |> mapAttrs (name: cfg: {
-          inherit (cfg) package jvmOpts serverProperties whitelist;
+        |> mapAttrs (name: server: {
+          inherit (server) files jvmOpts package serverProperties whitelist;
+
           enable = true;
+
+          symlinks = mkMerge [
+            (mkIf (server.modpack != null) (modpackSymlinks server.modpack))
+            server.symlinks
+          ];
         });
     };
 
